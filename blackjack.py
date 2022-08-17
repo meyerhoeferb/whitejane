1605155fca328c35f65d0c7f31d1e92eefcc2bc1
Braden Meyerhoefer <bredcurve@gmail.com>
2022-08-17 17:10:09 -0400

    Add logic for game setup and beginning of player turn

--- blackjack.py
+++ blackjack.py
@@ -1,26 +1,66 @@
-'''
+"""
 simple blackjack implementation
-'''
+"""
 
 import random
 
-class BlackJack():
+
+class BlackJack:
     def __init__(self):
         self.deck = self._shuffle_deck()
-        
-        self.hands = []         # 2d array tracking dealer and player hands
-        
-        self.dealer = 0             # dealer will be the first player
-        self.current_player = 1     # track player whose action it is
-        
-        
-    # internal methods   
-        
-    def _shuffle_deck(self) -> list[int]:
-        '''
-        returns a shuffled deck as a list of ints
-        '''
-        return random.shuffle([x for x in range(52)])
-        
 
-        
+        self.hands = []  # 2d array tracking dealer and player hands
+
+        self.num_players = 0  # gets set every time a hand starts
+
+        self.dealer = 0  # dealer will be the first player
+        self.current_player = 1  # track player whose action it is
+
+    # internal methods
+
+    def _shuffle_deck(self) -> list[int]:
+        """
+        returns a shuffled deck as a list of ints
+        """
+        deck = [x for x in range(52)]
+        random.shuffle(deck)
+        return deck
+
+    def _draw_card(self) -> int:
+        """
+        draw card from deck
+        """
+        return self.deck.pop()
+
+    def _deal_starting_hand(self) -> list[int]:
+        """
+        deal a starting hand of 2 cards
+        """
+        return [self._draw_card(), self._draw_card()]
+
+    # interface methods
+    def start_game(self, num_players: int = 1):
+        """
+        deal a new hand for however many players + the dealer
+
+        defaults to 1 player
+        """
+        self.deck = self._shuffle_deck()
+        self.num_players = num_players
+
+        # one more than num players (for dealer)
+        for i in range(num_players + 1):
+            self.hands.append(self._deal_starting_hand())
+
+    def get_player_hand(self, player: int = 0) -> list[int]:
+        """
+        returns the specified players hand as a list of ints
+        defaults to current player, cannot be used to get dealer hand
+        """
+        if player > self.num_players:
+            raise Exception(f"Tried to get player {player}s hand, but only {self.num_players} in game")
+
+        if player == 0:
+            return self.hands[self.current_player]
+        else:
+            return self.hands[player]
--- local_cli.py
+++ local_cli.py
@@ -1,37 +1,75 @@
-'''
+"""
 cli for playing blackjack locally, no server
 
 only single player
-'''
+"""
 from blackjack import BlackJack
 
+CARD_MAP = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+
 def cli():
-    #make game object
+    # make game object
     bj = BlackJack()
-  
-    #gameplay loop  
+
+    # gameplay loop
     play = True
     while play:
         play = main_menu()
         if play:
-            # play the hand
-            pass
-    
+            # deal hand
+            bj.start_game()
+            # player turn loop
+            while bj.current_player <= bj.num_players:
+                print(f"YOUR HAND: {get_player_hand(bj)}")
+                action = player_menu()
+
+                # handle action
+                if action == "stand":
+                    break
+
     print("Thanks for playing!")
-    
-    
+
+
+def card_int_to_string(card: int) -> str:
+    """
+    convert card from integer to string representation
+    """
+    return CARD_MAP[card % 13]
+
+
+def get_player_hand(bj: BlackJack) -> str:
+    """
+    gets the current players hand from the game object and displays it
+    """
+    hand = bj.get_player_hand()
+
+    hand_str = [card_int_to_string(c) for c in hand]
+
+    return " ".join(hand_str)
+
+
+def player_menu() -> str:
+    """
+    displays the options the player can take and returns it as a string
+    """
+    print("player menu goes here")
+
+    return "stand"
+
+
 def main_menu() -> bool:
-    '''
+    """
     main menu, prompts for starting a hand or quitting
-    
+
     returns true if starting a hand, false otherwise
-    '''
-    
+    """
+
     while True:
         ans = input("Start a hand? [y/n]: ")
-        if ans == 'y':
+        if ans == "y":
             return True
-        if ans == 'n':
+        if ans == "n":
             return False
         else:
             print("Unrecognized input")
